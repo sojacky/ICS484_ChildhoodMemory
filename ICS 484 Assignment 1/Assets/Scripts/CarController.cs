e790ebdac206016471b0319a26d80bac45b636e7
sojacky <sojacky@hawaii.edu>
2016-03-28 16:20:48 -1000

    Tetsuo Harano Tunnels Memory

---
+++ ICS 484 Assignment 1/Assets/Scripts/CarController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+    private Rigidbody rb;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0.0f, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
